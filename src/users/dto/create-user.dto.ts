e84f5077f02bfcb39179282ebd96de2d4e7c1fe9
Sotatek-AnhMai2 <anh.mai2@sotatek.com>
2022-02-10 17:26:11 +0700

    feat: add validate

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,9 +1,15 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsAlphanumeric, MaxLength, MinLength } from 'class-validator';
 
 export class CreateUserDto {
   @ApiProperty()
+  @IsAlphanumeric()
+  @MinLength(6)
+  @MaxLength(20)
   username: string;
 
   @ApiProperty()
+  @MinLength(6)
+  @MaxLength(20)
   password: string;
 }
